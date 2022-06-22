5a098712f1fb433b346b2866b2c6065a2c021da9
yudai1019 <yudaydayo@gmail.com>
2022-06-22 11:08:44 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         has_many :room_users
+         has_many :rooms, through: :room_users
+         has_many :messages
   validates :name, presence: true
 end
